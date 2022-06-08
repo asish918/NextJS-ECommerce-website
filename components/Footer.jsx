b3917256d0d15fa29d92eb3f7c7c2f51dcb06a9c
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-08 23:11:30 +0530

    Basic Styling for Header and Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,15 @@
 import react from "react";
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="footer-container">
+            <p>2022, Boat All Rights Reserved</p>
+            <p className="icons">
+                <AiFillInstagram />
+                <AiOutlineTwitter />
+            </p>
+        </div>
     )
 }
 
